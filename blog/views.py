5befcd1d668f4a71729d8917ca97cbaba103f5ac
Padack2 <ajuma777@naver.com>
2024-05-04 11:52:02 +0900

    0504 KMU TDD 수업 실습

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,36 @@
 from django.shortcuts import render
-
+from .models import Post
+from django.views.generic import ListView, DetailView
 # Create your views here.
+
+class PostList(ListView):
+    model = Post
+    ordering = '-pk'
+    template_name = 'blog/post_list.html'
+
+class PostDetail(DetailView):
+    model = Post
+    # template_name으로 지정하지 않으면 [모델명]_detail.html 자동인식
+    # template_name = 'blog/post_detail.html'
+
+# def index(request):
+#     posts = Post.objects.all().order_by('-pk')
+#
+#     return render(
+#         request,
+#         'blog/post_list_back.html',
+#         {
+#             'posts': posts,
+#         }
+#     )
+
+def single_post_page(request, pk):
+    post = Post.objects.get(pk=pk)
+
+    return render(
+        request,
+        'blog/single_post_page.html',
+        {
+            'post': post,
+        }
+    )
